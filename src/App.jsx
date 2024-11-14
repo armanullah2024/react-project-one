2ee739b5b78da7e7afe95dd6f56a5b84587b21e5
Arman Ullah <armanullah690@gmail.com>
2024-11-14 22:48:42 +0600

    Project Done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+  <div><h2>Learn React</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores fugit consequatur debitis numquam sit officiis, reiciendis, esse laudantium, quibusdam laboriosam quisquam quidem minus atque hic molestiae. Unde officia quaerat debitis amet. Aliquam est sit sapiente maxime, repellendus illum. Modi voluptatem magnam suscipit repellat quia sed alias asperiores voluptas possimus aliquam!</p>
+  <a href="#" Learn More></a>;
+  </div>
+  );
+}
+
+export default App
